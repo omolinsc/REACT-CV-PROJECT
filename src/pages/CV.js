62e002afdff09ad905875c0b96d6e9df630cdcdd
omolinsc <oriol.molins@bootcamp-upgrade.com>
2024-07-09 16:35:38 +0200

    FEAT: added KOTLIN and ANDROID STUDIO to knowledge

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,238 +1,248 @@
 export const CV = {
-    hero: {
-      firstName: "Oriol",
-      secondName: "Molins Casanovas",
-      adress: "Calle Mayor 24",
-      city: "25100 - Almacelles (LLEIDA)",
-      email: "omolinsc@gmail.com",
-      birthDate: "26/04/1985",
-      phone: "(+34) 649 653 597",
-      image: "/assets/images/profile.jpeg",
-      gitHub: "https://github.com/omolinsc",
-      linkedIn: "https://www.linkedin.com/in/oriolmolinscasanovas/",
-      jobObjective: "Full Stack Developer ( FrontEnd / BackEnd )",
-      aboutMe: [
-        {
-          info: "Full Stack Developer en crecimiento formado en UPGRADE HUB",
-        },
-        {
-          info: "Entusiasta de las nuevas tecnologias y de la programación",
-        },
-        {
-          info: "Jugador amateur de Pádel, jugando casi a diario",
-        },
-        {
-          info: "Aficionado al dibujo, el miniaturismo y la lectura",
-        },
-      ],
+  hero: {
+    firstName: "Oriol",
+    secondName: "Molins Casanovas",
+    adress: "Calle Mayor 24",
+    city: "25100 - Almacelles (LLEIDA)",
+    email: "omolinsc@gmail.com",
+    birthDate: "26/04/1985",
+    phone: "(+34) 649 653 597",
+    image: "/assets/images/profile.jpeg",
+    gitHub: "https://github.com/omolinsc",
+    linkedIn: "https://www.linkedin.com/in/oriolmolinscasanovas/",
+    jobObjective: "Full Stack Developer ( FrontEnd / BackEnd )",
+    aboutMe: [
+      {
+        info: "Full Stack Developer en crecimiento formado en UPGRADE HUB",
+      },
+      {
+        info: "Entusiasta de las nuevas tecnologias y de la programación",
+      },
+      {
+        info: "Jugador amateur de Pádel, jugando casi a diario",
+      },
+      {
+        info: "Aficionado al dibujo, el miniaturismo y la lectura",
+      },
+    ],
+  },
+  education: [
+    {
+      name: "ESO y Bachiller Tecnológico",
+      start: "1998",
+      date: "2004",
+      where: "I.E.S. Canigó (Almacelles)",
     },
-    education: [
-      {
-        name: "ESO y Bachiller Tecnológico",
-        start: "1998",
-        date: "2004",
-        where: "I.E.S. Canigó (Almacelles)",
-      },
-      {
-        name: "CFGS de Administración y Finanzas",
-        start: "2012",
-        date: "2014",
-        where: "Colegio Episcopal (Lleida)",
-      },
-      {
-        name: "Full Stack Developer Bootcamp",
-        start: "2022",
-        date: "2023",
-        where: "Upgrade Hub (Madrid)",
-      },
-    ],
-    experience: [
-      {
-        name: "Auxiliar Administrativo",
-        date: "07/2014 – 06/2015",
-        where: "Comunitària Administració i Serveis, S.L.",
-        description:
-          "Gestión de recibos/facturas vía CRM, entrada de datos en el medio informático, elaboración de contabilidad y balance de gastos/ingresos de los clientes (comunidades de vecinos).",
-      },
-      {
-        name: "Jefe de Almacén y Logística",
-        date: "04/2016 – 02/2018",
-        where: "CUPASA",
-        description:
-          "Gestión de entradas/salidas de mercancía (fitosanitarios y semillas de cereal), gestión de albaranes y organización logística de las entregas y recepciones de productos.",
-      },
-      {
-        name: "Comercial Técnico",
-        date: "02/2018 – 02/2019",
-        where: "Prefabricados OJEFER, S.L.",
-        description:
-          "Elaboración de presupuestos y planos en DraftSight, presentación de la documentación a los clientes y captación de ellos a puerta fría por las regiones sobre todo de Cataluña y Aragón",
-      },
-      {
-        name: "Front-End Developer",
-        date: "04/2023 – ... ",
-        where: "CLEVERPY Machine Learning, S.L.",
-        description:
-          "Desarrollador Front-End con React Typescript y Vite, utilizando tecnologías como Axios, RTKQuery, ReduxToolkit, ... y plataformas como Jira, BitBucket, GitLab, Figmna y Swagger entre otros.",
-      },
-    ],
-    languages: [
-        {
-            language: "English",
-            wrlevel: "9",
-            splevel: "9",
-            isNative: false,
-        },
-        {
-            language: "Español",
-            wrlevel: "15",
-            splevel: "15",
-            isNative: true,
-        },
-        {
-            language: "Catalán",
-            wrlevel: "15",
-            splevel: "15",
-            isNative: true,
-        },
-    ],
-    otherLanguages: [
-        {
-          language: "HTML",
-          level: "12",
-          icon: "/assets/icons/html.png",
-        },
-        {
-          language: "SCSS BEMIT",
-          level: "13",
-          icon: "/assets/icons/sass.png",
-        },
-        {
-          language: "JavaScript",
-          level: "12",
-          icon: "/assets/icons/js.png",
-        },
-        {
-          language: "Typescript",
-          level: "13",
-          icon: "/assets/icons/ts.png",
-        },
-        {
-          language: "C/C++",
-          level: "7",
-          icon: "/assets/icons/c.png",
-        },
-        {
-          language: "REACT",
-          level: "13",
-          icon: "/assets/icons/react.png",
-        },
-        {
-          language: "Vite",
-          level: "12",
-          icon: "/assets/icons/vite.svg",
-        },
-        {
-          language: "ReduxTK & RTKQuery",
-          level: "10",
-          icon: "/assets/icons/rtkquery.png",
-        },
-        {
-          language: "php",
-          level: "5",
-          icon: "/assets/icons/php.png",
-        },
-        {
-          language: "Node",
-          level: "10",
-          icon: "/assets/icons/node.png",
-        },
-        {
-          language: "Angular",
-          level: "10",
-          icon: "/assets/icons/angular.png",
-        },
-    ],
-    tools: [
-        {
-          tool: "GitHub",
-          level: "12",
-          icon: "/assets/icons/github.png",
-        },
-        {
-          tool: "GitLab",
-          level: "10",
-          icon: "/assets/icons/gitlab.png",
-        },
-        {
-          tool: "BitBucket",
-          level: "10",
-          icon: "/assets/icons/bitbucket.png",
-        },
-        {
-          tool: "Jira",
-          level: "12",
-          icon: "/assets/icons/jira.png",
-        },
-        {
-          tool: "Figma",
-          level: "9",
-          icon: "/assets/icons/figma.png",
-        },
-        {
-          tool: "Swagger",
-          level: "10",
-          icon: "/assets/icons/swagger.png",
-        },
-        {
-          tool: "Windows",
-          level: "10",
-          icon: "/assets/icons/windows.png",
-        },
-        {
-          tool: "Office",
-          level: "15",
-          icon: "/assets/icons/office.png",
-        },
-        {
-          tool: "MongoDB",
-          level: "8",
-          icon: "/assets/icons/mongodb.png",
-        },
-        {
-          tool: "Visual Studio Code",
-          level: "13",
-          icon: "/assets/icons/vsc.png",
-        },
-        {
-          tool: "Postman",
-          level: "10",
-          icon: "/assets/icons/postman.png",
-        },
-        {
-          tool: "DraftSight",
-          level: "10",
-          icon: "/assets/icons/draftsight.png",
-        },
-        {
-          tool: "Photoshop",
-          level: "9",
-          icon: "/assets/icons/photoshop.png",
-        },
-        {
-          tool: "Illustrator",
-          level: "8",
-          icon: "/assets/icons/illustrator.png",
-        },
-    ],
-    habilities: [
-      "Permiso B de Conducir",
-      "Pair Programming",
-      "Diseño Gráfico",
-      "Empatía",
-      "Redes Sociales",
-      "Creatividad",
-      "Paciencia",
-      "Efectividad",
-    ],
-  };
+    {
+      name: "CFGS de Administración y Finanzas",
+      start: "2012",
+      date: "2014",
+      where: "Colegio Episcopal (Lleida)",
+    },
+    {
+      name: "Full Stack Developer Bootcamp",
+      start: "2022",
+      date: "2023",
+      where: "Upgrade Hub (Madrid)",
+    },
+  ],
+  experience: [
+    {
+      name: "Auxiliar Administrativo",
+      date: "07/2014 – 06/2015",
+      where: "Comunitària Administració i Serveis, S.L.",
+      description:
+        "Gestión de recibos/facturas vía CRM, entrada de datos en el medio informático, elaboración de contabilidad y balance de gastos/ingresos de los clientes (comunidades de vecinos).",
+    },
+    {
+      name: "Jefe de Almacén y Logística",
+      date: "04/2016 – 02/2018",
+      where: "CUPASA",
+      description:
+        "Gestión de entradas/salidas de mercancía (fitosanitarios y semillas de cereal), gestión de albaranes y organización logística de las entregas y recepciones de productos.",
+    },
+    {
+      name: "Comercial Técnico",
+      date: "02/2018 – 02/2019",
+      where: "Prefabricados OJEFER, S.L.",
+      description:
+        "Elaboración de presupuestos y planos en DraftSight, presentación de la documentación a los clientes y captación de ellos a puerta fría por las regiones sobre todo de Cataluña y Aragón",
+    },
+    {
+      name: "Front-End Developer",
+      date: "04/2023 – ... ",
+      where: "CLEVERPY Machine Learning, S.L.",
+      description:
+        "Desarrollador Front-End con React Typescript y Vite, utilizando tecnologías como Axios, RTKQuery, ReduxToolkit, ... y plataformas como Jira, BitBucket, GitLab, Figmna y Swagger entre otros.",
+    },
+  ],
+  languages: [
+    {
+      language: "English",
+      wrlevel: "9",
+      splevel: "9",
+      isNative: false,
+    },
+    {
+      language: "Español",
+      wrlevel: "15",
+      splevel: "15",
+      isNative: true,
+    },
+    {
+      language: "Catalán",
+      wrlevel: "15",
+      splevel: "15",
+      isNative: true,
+    },
+  ],
+  otherLanguages: [
+    {
+      language: "HTML",
+      level: "12",
+      icon: "/assets/icons/html.png",
+    },
+    {
+      language: "SCSS BEMIT",
+      level: "13",
+      icon: "/assets/icons/sass.png",
+    },
+    {
+      language: "JavaScript",
+      level: "12",
+      icon: "/assets/icons/js.png",
+    },
+    {
+      language: "Typescript",
+      level: "13",
+      icon: "/assets/icons/ts.png",
+    },
+    {
+      language: "C/C++",
+      level: "7",
+      icon: "/assets/icons/c.png",
+    },
+    {
+      language: "REACT",
+      level: "13",
+      icon: "/assets/icons/react.png",
+    },
+    {
+      language: "Vite",
+      level: "12",
+      icon: "/assets/icons/vite.svg",
+    },
+    {
+      language: "ReduxTK & RTKQuery",
+      level: "10",
+      icon: "/assets/icons/rtkquery.png",
+    },
+    {
+      language: "php",
+      level: "5",
+      icon: "/assets/icons/php.png",
+    },
+    {
+      language: "Node",
+      level: "10",
+      icon: "/assets/icons/node.png",
+    },
+    {
+      language: "Angular",
+      level: "10",
+      icon: "/assets/icons/angular.png",
+    },
+    {
+      language: "Kotlin",
+      level: "8",
+      icon: "/assets/icons/kotlin.png",
+    },
+  ],
+  tools: [
+    {
+      tool: "GitHub",
+      level: "12",
+      icon: "/assets/icons/github.png",
+    },
+    {
+      tool: "GitLab",
+      level: "10",
+      icon: "/assets/icons/gitlab.png",
+    },
+    {
+      tool: "BitBucket",
+      level: "10",
+      icon: "/assets/icons/bitbucket.png",
+    },
+    {
+      tool: "Jira",
+      level: "12",
+      icon: "/assets/icons/jira.png",
+    },
+    {
+      tool: "Figma",
+      level: "9",
+      icon: "/assets/icons/figma.png",
+    },
+    {
+      tool: "Swagger",
+      level: "10",
+      icon: "/assets/icons/swagger.png",
+    },
+    {
+      tool: "Android Studio",
+      level: "10",
+      icon: "/assets/icons/android_studio.png",
+    },
+    {
+      tool: "Windows",
+      level: "10",
+      icon: "/assets/icons/windows.png",
+    },
+    {
+      tool: "Office",
+      level: "15",
+      icon: "/assets/icons/office.png",
+    },
+    {
+      tool: "MongoDB",
+      level: "8",
+      icon: "/assets/icons/mongodb.png",
+    },
+    {
+      tool: "Visual Studio Code",
+      level: "13",
+      icon: "/assets/icons/vsc.png",
+    },
+    {
+      tool: "Postman",
+      level: "10",
+      icon: "/assets/icons/postman.png",
+    },
+    {
+      tool: "DraftSight",
+      level: "10",
+      icon: "/assets/icons/draftsight.png",
+    },
+    {
+      tool: "Photoshop",
+      level: "9",
+      icon: "/assets/icons/photoshop.png",
+    },
+    {
+      tool: "Illustrator",
+      level: "8",
+      icon: "/assets/icons/illustrator.png",
+    },
+  ],
+  habilities: [
+    "Permiso B de Conducir",
+    "Pair Programming",
+    "Diseño Gráfico",
+    "Empatía",
+    "Redes Sociales",
+    "Creatividad",
+    "Paciencia",
+    "Efectividad",
+  ],
+};
